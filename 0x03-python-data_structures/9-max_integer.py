6e2c9ec054c19c0c8e4a9ac312073b3f9a98aa52
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-09-16 19:21:41 -0700

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        mx = 0
+        mx = my_list[0]
         for i in my_list:
             if i > mx:
                 mx = i
