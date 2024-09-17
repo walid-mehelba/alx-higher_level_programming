4208fd763a553ba9fdf462d91a42b7a2f449bb43
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-09-16 19:18:49 -0700

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        mx = 0
+        for i in my_list:
+            if i > mx:
+                mx = i
+    return mx
