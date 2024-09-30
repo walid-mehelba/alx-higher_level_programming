2924f4cf5e89dcbe1a5a461ecbeea45ab9eea45e
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-09-30 11:28:50 -0700

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i], end="")
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
             counter += 1
-        except IndexError:
-            break
-    print("")
-    return (counter)
+    except IndexError:
+        pass
+    print()
+    return counter
+
