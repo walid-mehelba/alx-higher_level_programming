870ca35640af54a1f06e3dc5b3e7241cd159aecf
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-11-19 16:50:35 -0800

    update

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Deletes the database hbtn-0c_0
+DROP database IF EXISTS hbtn_0c_0;
