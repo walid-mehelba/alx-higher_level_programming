da2d3769bd5186f79fb2e2483d8ec74f6e32b7b7
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-12-16 17:39:21 -0800

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-function fac (n) {
+function factorial (n) {
   if (n < 0) {
     return (-1);
   }
   if (n === 0 || isNaN(n)) {
     return (1);
   }
-  return (n * fac(n - 1));
+  return (n * factorial(n - 1));
 }
 
-
-console.log(fac(Number(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
